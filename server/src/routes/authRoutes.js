f73cd019115c0850b89d020240d4dd2636cb988a
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-03 22:01:55 +0530

    simulated requests using .rest file

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,7 +5,16 @@ const router = express.Router()
 
 
 
-router.post('/register' , (req , res) => {})
+router.post('/register' , (req , res) => {
+
+    const {username , password} = req.body
+    console.log(req.body)
+    res.json({
+        username : username,
+        password : password
+    })
+
+})
 
 
 router.post('/login' , (req , res) => {})
--- server/src/server.js
+++ server/src/server.js
@@ -8,9 +8,15 @@ const app = express()
 const PORT = 5000 || process.env.PORT
 
 
+
+app.use(express.json())
+
 app.use('/auth' , authRoutes)
 app.use('/todos' , todoRoutes)
 
+app.get('/' , (req , res) => {
+    res.status(200).json({message : "Working route "})
+})
 
 app.listen(PORT , ()=> {
     console.log(`Server started on port ${PORT}`)
