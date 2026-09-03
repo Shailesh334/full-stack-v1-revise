8ee8ea1cd87b44f847d19728913845bd02c3cbc6
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-03 21:47:45 +0530

    creating routes

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+
+const router = express.Router()
+
+
+
+router.post('/register' , (req , res) => {})
+
+
+router.post('/login' , (req , res) => {})
+
+
+export default router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 import express from "express"
-
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 
 
 const PORT = 5000 || process.env.PORT
+
+
+app.use('/auth' , authRoutes)
+app.use('/todos' , todoRoutes)
+
 
 app.listen(PORT , ()=> {
     console.log(`Server started on port ${PORT}`)
