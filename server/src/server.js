1c6b9c13a745b61bea0945740f34370db37939ec
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-03 19:18:44 +0530

    installed packages and started server

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+
+const app = express()
+
+
+const PORT = 5000 || process.env.PORT
+
+app.listen(PORT , ()=> {
+    console.log(`Server started on port ${PORT}`)
+})
