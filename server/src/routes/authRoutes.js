2b6e76259a25fee4900e76c20d91927ea9706294
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-06 14:10:00 +0530

    login an user logic

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,45 +1,70 @@
-import express from "express"
-import bcrypt from "bcrypt"
-import db from "../db.js"
-import jwt from "jsonwebtoken"
+import express from "express";
+import bcrypt from "bcrypt";
+import db from "../db.js";
+import jwt from "jsonwebtoken";
 
+const router = express.Router();
 
-const router = express.Router()
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
 
+  // create hashed password
+  const hashPassWord = bcrypt.hashSync(password, 8);
 
-
-router.post('/register' , (req , res) => {
-
-    const {username , password} = req.body
-    
-    // create hashed password
-    const hashPassWord = bcrypt.hashSync(password , 8);
-
-    try{
-        // Store user into db
-     const query = db.prepare(`
+  try {
+    // Store user into db
+    const query = db.prepare(`
         INSERT INTO users (username , password) VALUES (? , ?)
-        `)
-    
-    const result = query.run(username , hashPassWord);
+        `);
+
+    const result = query.run(username, hashPassWord);
 
     // create token
-    const token = jwt.sign({id : result.lastInsertRowid} , process.env.JWT_SECRET , {expiresIn : '24h'})
-    
-    // send token
-    res.json({token}) 
+    const token = jwt.sign(
+      { id: result.lastInsertRowid },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+    );
 
+    // send token
+    res.json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(502).json({ message: err.message });
+  }
+});
+
+router.post("/login", (req, res) => {
+    const {username , password} = req.body;
+
+    try{
+        // Get user from db
+        const getUser = db.prepare(`
+            SELECT * FROM users WHERE username = ?
+        `)
+        const user = getUser.get(username)
+        
+        // Check if user exist
+        if(!user){
+            return res.status(404).json({message : "User not found "})
+        }
+
+        // compare passwrd
+        const isPassValid = bcrypt.compareSync(password , user.password)
+
+        if(!isPassValid){
+            return res.status(401).send({message : "Invalid password "})
+        }
+
+        // generate token
+        const token = jwt.sign({id : user.id } , process.env.JWT_SECRET , {expiresIn : '24h'})
+        res.send({token})
+
+        
     }catch(err){
         console.log(err.message);
-        res.status(502).json({message : err.message})
+        res.status(503)
     }
-  
-
-    
-})
-
-
-router.post('/login' , (req , res) => {})
-
+});
 
 export default router;
