2dc339e5c69efd88dcb9fc4d31c3260147f953c2
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-07 22:20:19 +0530

    handle register (without token storing/handling)

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,11 +1,59 @@
+import { useState } from "react"
 
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
+ 
+   const [form , setForm] = useState({
+     username : "",
+     password : ""
+   })
+ 
+   const [error , setError] = useState(null)
+ 
+   const handleSubmit = async (e)=>{
+     e.preventDefault()
+     try{
+        
+       const response = await fetch("http://localhost:5000/auth/register" , {
+         method : "POST" ,
+         headers : {
+           "Content-Type" : "application/json"
+         },
+         body : JSON.stringify({
+           username : form.username ,
+           password : form.password
+         })
+       })
+ 
+       const data = await response.json()
+ 
+       if(!response.ok) {
+         setError(data.message);
+         return;
+       }
+ 
+       console.log(data)
+ 
+     }catch(err){
+       setError(err)
+     }
+   }
+   return (
+     <div className="min-h-screen flex items-center justify-center ">
+       <form className="bg-white p-6 shadow-md rounded-lg" onSubmit={handleSubmit}>
+ 
+         <h1 className="text-xl mb-4 ">Register</h1>
+         {error && <p className="text-red-500 mb-4">{error}</p>}
+         <input type="username" placeholder="username" className="border w-full p-2 mb-3 rounded-md" value={form.username} onChange={(e) => setForm({
+           ...form , username : e.target.value
+         })}/>
+          <input type="password" placeholder="password" className="border w-full p-2 mb-3 rounded-md" value={form.password} onChange={(e) => setForm({
+           ...form , password : e.target.value
+         })}/>
+         <button className="bg-blue-500 text-white w-full border p-2 mb-3 rounded-md cursor-pointer">Submit</button>
+       </form>
+     </div>
+   )
 }
 
 export default Register
