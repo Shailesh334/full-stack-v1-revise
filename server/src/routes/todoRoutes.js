a1853fe34c715136e05f470db80a0a2787f7d904
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-07 11:24:52 +0530

    created and tested Insert a todo route

--- server/src/routes/todoRoutes.js
+++ server/src/routes/todoRoutes.js
@@ -1,37 +1,46 @@
-import express from "express"
-import db from "../db.js"
+import express from "express";
+import db from "../db.js";
 
-const router = express.Router()
-
+const router = express.Router();
 
 // Get all todos
-router.get('/' , (req , res) => {
-    
-    try{
+router.get("/", (req, res) => {
+
+  try {
     const getTodos = db.prepare(`
         SELECT * FROM todos WHERE user_ID = ? 
-    `)
+    `);
 
-    const response = getTodos.all(req.userId)
+    const response = getTodos.all(req.userId);
 
-    res.json({ response})
-    }catch(err){
-        console.log(err.message)
-        res.status(503).json({message : err.message})
-    }
-   
-})
+    res.json({ response });
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(503).json({ message: err.message });
+  }
+});
 
 // Create a new todo
-router.post('/' , (req , res) => {})
+router.post("/", (req, res) => {
+    const {task} = req.body;
+
+    const insertTodo = db.prepare(`
+        INSERT INTO todos (user_id , task) VALUES (? , ?)
+    `)
+    const result = insertTodo.run(req.userId , task)
+
+    res.json({id : result.lastInsertRowid , task , completed : 0})
+    
+});
 
 // Fetch a single todo
-router.post('/:id' , (req , res) => {})
+router.post("/:id", (req, res) => {});
 
 // Update a  todo
-router.put('/:id' , (req , res) => {})
+router.put("/:id", (req, res) => {});
 
 // Delete a todo
-router.delete('/:id' , (req , res) => {})
+router.delete("/:id", (req, res) => {});
 
 export default router;
